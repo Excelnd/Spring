a0b668b18f548e293df5cfe57f3bc542baca49b0
ihsan <ihsanmpm@gmail.com>
2020-03-12 17:10:48 +0530

    add

---
+++ spring-boot/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.RestController;
+
+//import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/")
+}
